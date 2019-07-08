f1566410ffc734b4f5e9aa53ca090b3aa31ff25d
s <s@MacBook-Air-alkhas-b-s.local>
2019-07-09 02:40:18 +0300

    Ahmed AlMeshaal

---
+++ loopArray.rb
@@ -0,0 +1,165 @@
+# Return an array of each Student's full name, upper-cased
+#  students = [
+#    {
+#        first_name: 'Ahmed',
+#        last_name: 'Al-Bahrani'
+#    },
+#    {
+#        first_name: 'Reem',
+#        last_name: 'AlHarbi',
+#    },
+#    {
+#        first_name: 'Mansour',
+#        last_name: 'Almohsen',
+#    }
+# ]
+
+# uppercase = students.map{|i| i.values[0].upcase + i.values[1].upcase} 
+# uppercase.map {|i| puts i }
+
+
+
+# Find the first order for each user
+
+
+# users = [
+#   {
+#       name: 'Hanan',
+#       orders: [
+#           {
+#               description: 'a bike'
+#           }
+#       ]
+#   },
+#   {
+#       name: 'Abdulrahman',
+#       orders: [
+#           {
+#               description: 'bees'
+#           },
+#           {
+#               description: 'fishing rod'
+#           }
+#       ]
+#   },
+#   {
+#       name: 'Turki',
+#       orders: [
+#           {
+#               description: 'a MacBook'
+#           },
+#           {
+#               description: 'The West Wing DVDs'
+#           },
+#           {
+#               description: 'headphones'
+#           },
+#           {
+#               description: 'a kitten'
+#           }
+#       ]
+#   }
+# ]
+
+# p users.map {|x| x.values[1][0]}
+
+
+
+# people = [
+#   {
+#       name: 'Ahlam',
+#       transactions: [
+#           {
+#               type: 'COFFEE',
+#               amount: 7.43
+#           },
+#           {
+#               type: 'TACOS',
+#               amount: 14.65
+#           },
+#           {
+#               type: 'COFFEE',
+#               amount: 4.43
+#           }
+#       ]
+#   },
+#   {
+#       name: 'Sulaiman',
+#       transactions: [
+#           {
+#               type: 'BIKES',
+#               amount: 800.00
+#           },
+#           {
+#               type: 'TACOS',
+#               amount: 14.65
+#           },
+#           {
+#               type: 'COFFEE',
+#               amount: 4.43
+#           }
+#       ]
+#   },
+#   {
+#       name: 'Norah',
+#       transactions: [
+#           {
+#               type: 'COFFEE',
+#               amount: 7.43
+#           },
+#           {
+#               type: 'COFFEE',
+#               amount: 100.00
+#           },
+#           {
+#               type: 'COFFEE',
+#               amount: 4.43
+#           }
+#       ]
+#   }
+# ]
+
+# stores = [
+#   {
+#       store_name: 'Jarir',
+#       products: [
+#           {
+#               description: 'Titanium',
+#               price: 9384.33
+#           },
+#           {
+#               description: 'Gold',
+#               price: 345.54
+#           }
+#       ]
+#   },
+#   {
+#       store_name: 'Tamimi',
+#       products: [
+#           {
+#               description: 'Silver',
+#               price: 654.44
+#           },
+#           {
+#               description: 'Ruby',
+#               price: 323.43
+#           }
+#       ]
+#   },
+#   {
+#       store_name: 'Souq',
+#       products: [
+#           {
+#               description: 'Opal',
+#               price: 345.43
+#           },
+#           {
+#               description: 'Sapphire',
+#               price: 899.33
+#           }
+#       ]
+#   }
+# ]
+
+  
+   
