f32eac4c9d3effb9c7c578eb154ffa913d3921ba
Abdullah <a_t1411@hotmail.com>
2019-07-08 23:00:20 +0300

    HW W04D02

---
+++ main.rb
@@ -0,0 +1,197 @@
+# Q1
+students = [
+  {
+      first_name: 'Ahmed',
+      last_name: 'Al-Bahrani'
+  },
+  {
+      first_name: 'Reem',
+      last_name: 'AlHarbi',
+  },
+  {
+      first_name: 'Mansour',
+      last_name: 'Almohsen',
+  }
+]
+
+arr=students.map do |h|
+h.map do |_key,val|
+ upper=val.upcase
+end
+end
+index=0
+arr.map do |a|
+
+
+puts upper_case_full_names = arr[index].join(" ")
+index +=1
+end
+
+#Q2
+users = [
+  {
+      name: 'Hanan',
+      orders: [
+          {
+              description: 'a bike'
+          }
+      ]
+  },
+  {
+      name: 'Abdulrahman',
+      orders: [
+          {
+              description: 'bees'
+          },
+          {
+              description: 'fishing rod'
+          }
+      ]
+  },
+  {
+      name: 'Turki',
+      orders: [
+          {
+              description: 'a MacBook'
+          },
+          {
+              description: 'The West Wing DVDs'
+          },
+          {
+              description: 'headphones'
+          },
+          {
+              description: 'a kitten'
+          }
+      ]
+  }
+]
+3.times { |i| puts users[i][:orders][0] }
+
+# Q3
+
+people = [
+  {
+      name: 'Ahlam',
+      transactions: [
+          {
+              type: 'COFFEE',
+              amount: 7.43
+          },
+          {
+              type: 'TACOS',
+              amount: 14.65
+          },
+          {
+              type: 'COFFEE',
+              amount: 4.43
+          }
+      ]
+  },
+  {
+      name: 'Sulaiman',
+      transactions: [
+          {
+              type: 'BIKES',
+              amount: 800.00
+          },
+          {
+              type: 'TACOS',
+              amount: 14.65
+          },
+          {
+              type: 'COFFEE',
+              amount: 4.43
+          }
+      ]
+  },
+  {
+      name: 'Norah',
+      transactions: [
+          {
+              type: 'COFFEE',
+              amount: 7.43
+          },
+          {
+              type: 'COFFEE',
+              amount: 100.00
+          },
+          {
+              type: 'COFFEE',
+              amount: 4.43
+          }
+      ]
+  }
+]
+people.map do |arr|
+
+    total=[]
+    arr[:transactions].map do |arr2|
+    if arr2[:type] =="COFFEE"
+     total.push(arr2[:amount])
+    
+    end
+    end
+    
+    p name = arr[:name]
+    new_total = total.reduce(:+)
+    p the_average =new_total/ total.length
+    
+    end
+# I locked up this code after I tried the problem for an hour
+
+# Q4
+
+stores = [
+  {
+      store_name: 'Jarir',
+      products: [
+          {
+              description: 'Titanium',
+              price: 9384.33
+          },
+          {
+              description: 'Gold',
+              price: 345.54
+          }
+      ]
+  },
+  {
+      store_name: 'Tamimi',
+      products: [
+          {
+              description: 'Silver',
+              price: 654.44
+          },
+          {
+              description: 'Ruby',
+              price: 323.43
+          }
+      ]
+  },
+  {
+      store_name: 'Souq',
+      products: [
+          {
+              description: 'Opal',
+              price: 345.43
+          },
+          {
+              description: 'Sapphire',
+              price: 899.33
+          }
+      ]
+  }
+]
+most_expensive_products_by_store = stores.map do |arr_store|
+item_price = arr_store[:products].max_by do |item|
+item[:price]
+end
+{
+
+  name: arr_store[:store_name],
+  description:item_price[:description],
+  expensive_products: item_price[:price]
+}
+end
+
